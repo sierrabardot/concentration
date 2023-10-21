90391af40ef51507de0efec909e5ee2c801ca215
Sierra Sutherland-Brown <sierrabardot@live.com.au>
2023-10-21 17:24:28 +1100

    Create JS file and declare game state  variables

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+/*---------- Constants ----------*/
+
+/*---------- Declare variables ----------*/
+let gameStatus = {
+    board: [],
+    score: 0,
+    moves: 0,
+    inPlay: false,
+};
+
+/*---------- Cache HTML elements ----------*/
+
+/*---------- Initialize ----------*/
+
+/*---------- Event listeners ----------*/
+
+/*---------- Functions ----------*/
