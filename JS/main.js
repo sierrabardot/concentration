df4977626074dece5841aa904360034a909c1c1b
Sierra Sutherland-Brown <sierrabardot@live.com.au>
2023-10-21 18:13:00 +1100

    Start assignRandomSquares function

--- JS/main.js
+++ JS/main.js
@@ -12,7 +12,8 @@ let gameState = {
 
 /*---------- Initialize ----------*/
 function init() {
-    board = assignRandomSquares();
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    assignRandomSquares();
     gameState.inPlay = true;
     render();
 }
@@ -20,4 +21,18 @@ function init() {
 /*---------- Event listeners ----------*/
 
 /*---------- Functions ----------*/
-function assignRandomSquares() {}
+function assignRandomSquares() {
+    const iconNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for (let i = 0; i < gameState.board.length; i++) {}
+}
+
+function render() {
+    renderBoard();
+    renderMessage();
+}
+
+function renderBoard() {
+    for (let i = 0; i < board.length; i++) {
+        const idSelector = `#square-${i}`;
+    }
+}
